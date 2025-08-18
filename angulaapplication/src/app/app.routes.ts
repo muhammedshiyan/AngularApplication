b2e720de74092e92d00fbd3f8741e0cfbfc92c86
muhammedshiyanv <muhammedshiyanv@gmail.com>
2025-08-19 02:34:33 +0530

    created nav bar in layout page and route for that dash board login profile pages

--- angulaapplication/src/app/app.routes.ts
+++ angulaapplication/src/app/app.routes.ts
@@ -2,23 +2,20 @@ import { Routes } from '@angular/router';
 import { Logincomponent  } from './pages/login/login.component';
 import { Layoutcomponent  } from './pages/layout/layout.component';
 import { Dashboardcomponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent  } from './pages/profile/profile.component';
 
 export const routes: Routes = [
-    {
-        path:'',redirectTo:'login',pathMatch:'full'
-    },
-    {
-        path:'login',
-        component:Logincomponent    
-    }
-    ,
-    {
-       path:'',
-       component:Layoutcomponent,
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'login',component:Logincomponent},
+    {path:'',component:Layoutcomponent,
        children:[
         {
             path:'dashboard',
             component:Dashboardcomponent
+        },
+        {
+        path: 'profile',
+        component: ProfileComponent
         }
        ]
     }
--- angulaapplication/src/app/pages/layout/layout.component.ts
+++ angulaapplication/src/app/pages/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
