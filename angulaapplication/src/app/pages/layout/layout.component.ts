2839cee99b524724b6c9da303b9c902ead096e97
muhammedshiyanv <muhammedshiyanv@gmail.com>
2025-08-19 02:41:10 +0530

    basic log out fn implimented

--- angulaapplication/src/app/pages/layout/layout.component.ts
+++ angulaapplication/src/app/pages/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet,RouterLink, RouterLinkActive,Router  } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -9,5 +9,14 @@ import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './layout.component.css'
 })
 export class Layoutcomponent  {
+constructor(private router: Router) {}
 
+  logout() {
+    // 🔹 Clear stored token/session
+    localStorage.removeItem('token'); 
+    sessionStorage.clear();
+
+    // 🔹 Navigate to login
+    this.router.navigate(['/login']);
+  }
 }
